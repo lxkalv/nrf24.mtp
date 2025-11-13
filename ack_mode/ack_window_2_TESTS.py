58761018a311763cda86ccd8e5a9c173160e6ec9
PauTalarn <pau.talarn@gmail.com>
2025-11-13 20:59:07 +0100

    sdfas

--- ack_mode/ack_window_2_TESTS.py
+++ ack_mode/ack_window_2_TESTS.py
@@ -36,7 +36,7 @@ MAX_ATTEMPTS  = 1000               # <<< per-packet retries (you can adjust)
 ID_WIND_BYTES=3
 ID_CHUNK_BYTES=1
 PAYLOAD_SIZE=32
-WINDOW_SIZE = 10
+WINDOW_SIZE = 3
 SEQ_START   = 1        # first packet ID
 ACK_WAIT   = 0.005      # time to wait for an ACK (s)
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
