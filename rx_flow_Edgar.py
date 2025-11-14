be653a7c522128fad6557e29d1ef652c2adb38b9
PauTalarn <pau.talarn@gmail.com>
2025-11-14 17:44:54 +0100

    df

--- rx_flow_Edgar.py
+++ rx_flow_Edgar.py
@@ -149,9 +149,7 @@ def RX_LINK_LAYER(PRX: CustomNRF24) -> None:
 
         # Pull the received frame from the FIFO
         frame = PRX.get_payload()
-
-        length = len(frame)
-        INFO(f"[RX] len={length:02d} data={frame.hex()}")
+        INFO(f"Burst sent {frame.hex()}")
         # NOTE: If the first Byte has the format 11110000 then it is a TRANSFER_INFO
         # message. After we have received this type of message we generate the emtpy
         # STREAM structure with all the allocated slots where we will store each
