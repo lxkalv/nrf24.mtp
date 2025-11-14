18c7cfba0b636901ea6aa2ae9434307b8ea58f3b
rued86 <edgarhr2408@live.com>
2025-11-14 23:22:07 +0100

    2mbps

--- radio.py
+++ radio.py
@@ -49,8 +49,8 @@ class Role(Enum):
 # :::: RADIO CONFIG :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 CE_PIN               = 22
 CHANNEL              = 76                          # NOTE: Select one from [0..125], Channel is computed as 2.4GHz + (CHANNEL)MHz
-DATA_RATE            = RF24_DATA_RATE.RATE_1MBPS   # NOTE: Select one from {250KBPS, 1MBPS, 2MBPS}
-PA_LEVEL             = RF24_PA.LOW                 # NOTE: Select one from {MIN (-18dBm), LOW (-12dBm), HIGH (-6dBm), MAX (0dBm)}
+DATA_RATE            = RF24_DATA_RATE.RATE_2MBPS   # NOTE: Select one from {250KBPS, 1MBPS, 2MBPS}
+PA_LEVEL             = RF24_PA.HIGH                 # NOTE: Select one from {MIN (-18dBm), LOW (-12dBm), HIGH (-6dBm), MAX (0dBm)}
 CRC_BYTES            = RF24_CRC.BYTES_2            # NOTE: Select one from {DISABLED, BYTES_1, BYTES_2}
 PAYLOAD_SIZE         = RF24_PAYLOAD.ACK            # NOTE: Select one from {ACK, DYNAMIC, MIN (1), MAX (32), [1..32]}
 RETRANSMISSION_TRIES = 15                          # NOTE: Select one from [1..15]
