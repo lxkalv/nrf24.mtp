ee6e3906aacabee2f3bb2d52e5a15c96d581cc90
lxkalv <lucas.alvarez.fernandez.0@gmail.com>
2025-11-19 00:10:33 +0100

    changed args

--- test_suite/module/layers_ApplicationLayer_configure_radio_object.py
+++ test_suite/module/layers_ApplicationLayer_configure_radio_object.py
@@ -1,18 +1,18 @@
 # :::: IMPORTS ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 import sys
 
-import nrf24_mtp.utils.Logger as Logger
-
 import nrf24_mtp.layers.ApplicationLayer as ApplicationLayer
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 
 # :::: configure_radio_object() test ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-if True:
-    sys.argv = ["layers_ApplicationLayer_configure_radio_object.py", "--mode", "TX", "--print-config"]
-    Logger.INFO("Test with default config")
-    config = ApplicationLayer.parse_arguments("Test")
-    radio  = ApplicationLayer.configure_radio_object(config)
-    radio.show_registers()
+sys.argv = [
+    "layers_ApplicationLayer_configure_radio_object.py",
+    "--mode", "TX",
+    "--print-config",
+]
+config = ApplicationLayer.parse_arguments("Test")
+radio  = ApplicationLayer.configure_radio_object(config)
+radio.show_registers()
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
