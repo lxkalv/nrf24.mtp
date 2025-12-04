da5a6c0ce1d014b547d6eb7c4a67c4f13bbe4c43
PauTalarn <pau.talarn@gmail.com>
2025-12-04 17:55:22 +0100

    sdfa

--- orchestrator.py
+++ orchestrator.py
@@ -5,11 +5,61 @@ import threading
 import os
 from pathlib import Path
 
-from utils import (
-    YELLOW,
-    ERROR,
-    INFO,
-)
+# :::: COLORING FUNCTIONS :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+def RED(message: str) -> str:
+    """
+    Returns a copy of the string wrapped in ANSI scape sequences to make it red
+    """
+    return f"\033[31m{message}\033[0m"
+
+def GREEN(message: str) -> str:
+    """
+    Returns a copy of the string wrapped in ANSI scape sequences to make it green
+    """
+    return f"\033[32m{message}\033[0m"
+
+def YELLOW(message: str) -> str:
+    """
+    Returns a copy of the string wrapped in ANSI scape sequences to make it yellow
+    """
+    return f"\033[33m{message}\033[0m"
+
+def BLUE(message: str) -> str:
+    """
+    Returns a copy of the string wrapped in ANSI scape sequences to make it blue
+    """
+    return f"\033[34m{message}\033[0m"
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+
+
+# :::: MESSAGING FUNCTIONS ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+def ERROR(message: str, end = "\n") -> None:
+    """
+    Prints a message to the console with the red prefix `[~ERR]:`
+    """
+    print(f"{RED('[~ERR]:')} {message}", end = end)
+
+def SUCC(message: str, end = "\n") -> None:
+    """
+    Prints a message to the console with the green prefix `[SUCC]:`
+    """
+    print(f"{GREEN('[SUCC]:')} {message}", end = end)
+
+def WARN(message: str, end = "\n") -> None:
+    """
+    Prints a message to the console with the yellow prefix `[WARN]:`
+    """
+    print(f"{YELLOW('[WARN]:')} {message}", end = end)
+
+def INFO(message: str, end = "\n") -> None:
+    """
+    Prints a message to the console with the blue prefix `[INFO]:`
+    """
+    print(f"{BLUE('[INFO]:')} {message}", end = end)
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 # --- CUSTOM EXCEPTION ---
 class SoftReset(Exception):
