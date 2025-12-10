76127edbc6bfaa1a10666edeefad64ea62ed15ff
Rued86 <edgarhr2408@live.com>
2025-12-10 15:06:17 +0100

    .venv execution in orchestrator

--- orchestrator.py
+++ orchestrator.py
@@ -188,7 +188,7 @@ def RX_flow(scenario) :
 
     if scenario == "P2P":
         INFO("[State] Simple mode (P2P). Recieving P2P...")
-        cmd = "/usr/bin/python3.13 p2p.py"
+        cmd = "~/.venv/bin/python p2p.py"
         INFO(f"Executing: {cmd}")
         # result = os.system(cmd)
         result = subprocess.run(cmd, text=True)
