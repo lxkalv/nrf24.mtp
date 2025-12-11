19b5a10b1c7d6ddcd778ca9a522270fd9d502420
lxkalv <lucas.alvarez.fernandez.0@gmail.com>
2025-12-12 00:16:31 +0100

    reference point is p2p short

--- nm.py
+++ nm.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from math import ceil
 import argparse
 import pigpio
-import zlib
 import time
 import os
 
@@ -27,15 +26,22 @@ from typing import NoReturn
 
 
 # :::: CONSTANTS/GLOBALS ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-CE_PIN = 22
+CE_PIN_A                    = 22
+CE_PIN_B                    = 25
 
-BYTES_IN_FRAME              = 30
+BYTES_IN_FRAME              = 31
 
-CHANNEL_READ_TIMEOUT        = 200e-3
-NUMBER_OF_CYCLES            = 5
+CHANNEL_READ_TIMEOUT        = 500e-3
+PERSEVERANCE                = 50
+CHANNEL_PERMANENCE_TIMEOUT  = 3
+NUMBER_OF_CYCLES            = 10
 
-FILE_TO_TX_STR              = "MTP-F25-SRI-A-TX.txt"
-FILE_TO_RX_STR              = "MTP-F25-SRI-A-RX.txt"
+RECEIVED_FILE_NAME          = "MTP-F25-NM-A-RX.txt"
+
+TAN0_CHANNELS               = [76]
+TAN1_CHANNELS               = [76] # [ 5, 25, 45, 65, 85, 105]
+TBN0_CHANNELS               = [76] # [10, 30, 50, 70, 90, 110]
+TBN1_CHANNELS               = [76] # [15, 35, 55, 75, 95, 115]
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
@@ -65,12 +71,24 @@ def get_id() -> str:
 
 
 
-def disable_auto_ack(nrf: NRF24) -> None:
-    #nrf.unset_ce()
-    #nrf._nrf_write_reg(nrf.EN_AA, 0x00)   # <<< disable auto-ack for all pipes
-    #nrf.set_ce()
+def get_CE_pin(node_id: str) -> int:
+    """
+    Return the corresponding CE pin based on the node ID
+    """
+    if "tan" in node_id:
+        return CE_PIN_A
+    elif "tbn" in node_id:
+        return CE_PIN_B
 
-    nrf.set_retransmission(1, 15)  # <<< disable auto-retransmissions (x+1) * 250 µs
+
+
+def disable_auto_ack(nrf: NRF24) -> None:
+    nrf.unset_ce()
+    nrf._nrf_write_reg(nrf.EN_AA, 0x00)   # <<< disable auto-ack for all pipes
+    time.sleep(0.01)
+    nrf.set_ce()
+
+    # nrf.set_retransmission(0, 0)  # <<< disable auto-retransmissions (x+1) * 250 µs
     return
 
 
@@ -95,11 +113,12 @@ def create_radio_object(ce_pin: int) -> NRF24 | None:
         payload_size  = RF24_PAYLOAD.DYNAMIC,
         address_bytes = 4,
         crc_bytes     = RF24_CRC.BYTES_2,
-        pa_level      = RF24_PA.MAX,                # NOTE: Maybe increase this to MAX
+        pa_level      = RF24_PA.MAX,               # NOTE: Maybe increase this to MAX
     )
 
     # Shared address across all network nodes to simulate broadcast
-    address = b"NMNA"
+    address = b"NMND"
+    INFO(f"Selected address {address}")
     nrf.open_writing_pipe(address)
     nrf.open_reading_pipe(RF24_RX_ADDR.P1, address)
 
@@ -133,7 +152,7 @@ def get_node_config() -> tuple[NRF24 | None, str, bool]:
     node_id = get_id()
     INFO(f"Detected NODE_ID: {node_id}")
 
-    ce_pin = CE_PIN
+    ce_pin  = get_CE_pin(node_id)
     INFO(f"Selected CE PIN: {ce_pin}")
 
     nrf = create_radio_object(ce_pin)
@@ -142,45 +161,100 @@ def get_node_config() -> tuple[NRF24 | None, str, bool]:
 
 
 
-# :::: FILE IO ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-def get_file_to_transmit() -> Path | None:
+# :::: USB IO :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+USB_MOUNT_PATH = Path("/media")
+
+def get_usb_mount_path() -> Path | None:
     """
-    Get the path to the file to transmit
+    Try to find a valid USB device connected to the USB mount path
     """
     
-    file_path = Path(FILE_TO_TX_STR).resolve()
-    print(file_path)
-    if file_path.exists():
-        SUCC(f"Valid file detected inside USB: {file_path}")
-        return file_path
+    for path, _, _ in USB_MOUNT_PATH.walk():
+        if path.is_mount():
+            return path
+
+    return None
+
+
+def find_valid_txt_file_in_usb(usb_mount_path: Path) -> Path | None:
+    """
+    Searches for all the txt files in the first level of depth of the USB mount
+    location and returns the path to first one ordered alphabetically
+    """
+    if not usb_mount_path:
+        return None
     
-    else:
-        ERROR(f"No valid file was found inside the USB, stopping")
+    file = [
+        file
+        for file in usb_mount_path.iterdir()
+        if file.is_file()
+        and file.suffix == ".txt"
+        and not file.name.startswith(".")
+    ]
+
+    file = sorted(file)
+
+    if not file:
         return None
 
+    return file[0].resolve()
 
 
-def compress_file(content: bytes) -> bytes:
+def handle_tx_file_based_on_node_id(node_id: str) -> Path | None:
     """
-    Compress the file content using zlib
+    Handle the path to the file to transmit based on the NODE_ID
     """
-    compressed_content = zlib.compress(content, level = 6)
-    INFO(f"Compressed file from {len(content)} B to {len(compressed_content)} B")
-    return compressed_content
+    if "tan" in node_id:
+        file_path = Path("MTP-F25-NM-TX.txt").resolve()
+        if file_path.exists():
+            SUCC(f"Valid file detected inside USB: {file_path}")
+        else:
+            ERROR(f"No valid file was found inside the USB, stopping")
+        return file_path
+    
+    elif "tbn" in node_id:
+        INFO("Waiting for a valid USB")
+        usb_path = get_usb_mount_path()
+        while usb_path is None:
+            usb_path = get_usb_mount_path()
+            time.sleep(.05) # Wait 50 ms between checks
+        SUCC("USB detected")
 
+        INFO("Looking for a valid file inside the USB")
+        file_path = find_valid_txt_file_in_usb(usb_path)
 
-
-def decompress_file(content: bytes) -> bytes:
-    """
-    Decompress the file content using zlib
-    """
-    decompressed_content = zlib.decompress(content)
-    INFO(f"Decompressed file from {len(content)} B to {len(decompressed_content)} B")
-    return decompressed_content
+        if file_path:
+            SUCC(f"Valid file detected inside USB: {file_path}")
+        else:
+            ERROR(f"No valid file was found inside the USB, stopping")
+        return file_path
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 # :::: CHANNELS :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# def get_channels_based_on_node_id(node_id: str) -> tuple[list[int], list[int]]:
+#     """
+#     Return a list with the own channels and a list with the channels assigned to the other nodes
+#     """
+# 
+#     own_channels: list[int]   = []
+#     other_channels: list[int] = []
+# 
+#     if   node_id == "tan0":
+#         own_channels = TAN0_CHANNELS
+#         other_channels = TAN1_CHANNELS + TBN0_CHANNELS + TBN1_CHANNELS
+#     elif node_id == "tan1":
+#         own_channels = TAN1_CHANNELS
+#         other_channels = TAN0_CHANNELS + TBN0_CHANNELS + TBN1_CHANNELS
+#     elif node_id == "tbn0":
+#         own_channels = TBN0_CHANNELS
+#         other_channels = TAN0_CHANNELS + TAN1_CHANNELS + TBN1_CHANNELS
+#     elif node_id == "tbn1":
+#         own_channels = TBN1_CHANNELS
+#         other_channels = TAN0_CHANNELS + TAN1_CHANNELS + TBN0_CHANNELS
+# 
+#     return own_channels, other_channels
+
 def get_channels_based_on_node_id(all_channels: list[int], node_id: str) -> tuple[list[int], list[int]]:
     """
     Return a list with the own channels and a list with the channels assigned to the other nodes
@@ -227,7 +301,7 @@ def choose_free_channel(nrf: NRF24, own_channels: list[int]) -> int:
     for _ in range(NUMBER_OF_CYCLES):
         for idx, channel in enumerate(own_channels):
             nrf.set_channel(channel)
-            time.sleep(.1) # Wait for 200 ms
+            time.sleep(.2) # Wait for 200 ms
             channel_occupancy[idx] += is_channel_free(nrf)
     SUCC("Channel scan completed")
 
@@ -246,13 +320,12 @@ def choose_free_channel(nrf: NRF24, own_channels: list[int]) -> int:
 
 
 
-def choose_occupied_channel(nrf: NRF24, other_channels: list[int]) -> int:
+def choose_occupied_channel(nrf: NRF24, other_channels: list[int], channel_idx: int) -> tuple[int, int]:
     """
     Listen to RX channels to detect a frame in any channel
     """
     INFO("Listening to RX channels to look for transmitters")
     
-    channel_idx = 0
     while True:
         channel = other_channels[channel_idx % len(other_channels)]
         INFO(f"Listening on channel: {channel}")
@@ -267,13 +340,14 @@ def choose_occupied_channel(nrf: NRF24, other_channels: list[int]) -> int:
             if not nrf.data_ready(): continue
             
             INFO(f"Detected a transmitter on channel {channel}")
-            return channel
+            return channel, channel_idx
+
         channel_idx += 1
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 # :::: FLOW FUNCTIONS :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-def ACT_AS_TX(nrf: NRF24, content: bytes, own_channels: list[int]) -> NoReturn:
+def ACT_AS_TX(nrf: NRF24, node_id: str, content: bytes, own_channels: list[int], is_first_node: bool) -> NoReturn:
     """
     Put the node in TX mode and start transmitting indefinetly until the process is terminated
     """
@@ -284,15 +358,15 @@ def ACT_AS_TX(nrf: NRF24, content: bytes, own_channels: list[int]) -> NoReturn:
     
     # split the bytes into frames with a FrameID
     frames = [
-        FrameID.to_bytes(2) + content[i : i + BYTES_IN_FRAME]
+        FrameID.to_bytes(1) + content[i : i + BYTES_IN_FRAME]
         for FrameID, i in enumerate(range(0, len(content), BYTES_IN_FRAME))
     ]
 
     header_message  = bytes()
-    header_message += 0xFFFF.to_bytes(2)            # Header reserved to control messages NOTE: Up to 65.535 Data Frames
-    header_message += len(content).to_bytes(3)      # Ammount of data to transmit         NOTE: Up to 16.777.216 Bytes
-    header_message += shake_256(content).digest(27) # Checksum of the file
-    INFO(f"Generated header message: HEADER: {header_message[0:2]} | File length: {int.from_bytes(header_message[2:5])} B | Checksum: {header_message[5:].hex()}")
+    header_message += 0xFF.to_bytes(1)              # Header reserved to control messages NOTE: Up to  7.905 Bytes
+    header_message += len(content).to_bytes(2)      # Ammount of data to transmit         NOTE: Up to 65.536 Bytes
+    header_message += shake_256(content).digest(29) # Checksum of the file
+    INFO(f"Generated header message: HEADER: {header_message[0].to_bytes(1)} | File length: {int.from_bytes(header_message[1:3])} B | Checksum: {header_message[3:].hex()}")
     
     cycle = []
     cycle.append(header_message)
@@ -304,39 +378,48 @@ def ACT_AS_TX(nrf: NRF24, content: bytes, own_channels: list[int]) -> NoReturn:
         message = cycle[idx % cycle_len]
         nrf.send(message)
         idx += 1
+        
+
+        # if "tbn" in node_id and not is_first_node:
+        #     usb_mount_path = get_usb_mount_path()
+        #     if usb_mount_path:
+        #         INFO("SE HA ENCONTRADO UN USB PA GUARDAR LAS COSAS ERMANIKO") 
+        #         (usb_mount_path / RECEIVED_FILE_NAME).write_bytes(content)
+        #         is_first_node = True # No es que sea el primer nodo, pero como ya ha guardado el archivo lo pongo en TRUE porque ya ha cumplido su funcion
+        #         SUCC("ARCHIVO GUARDADO EN EL USB")
 
 
-
-
-
-def ACT_AS_RX(nrf: NRF24, other_channels: list[int]) -> tuple[bytes, float]:
+def ACT_AS_RX(nrf: NRF24, other_channels: list[int]) -> bytes:
     nrf.power_up_rx()
-    channel = choose_occupied_channel(nrf, other_channels)
+    channel, channel_idx = choose_occupied_channel(nrf, other_channels, 0)
     nrf.set_channel(channel)
-
-    tic_started        = False
 
     checksum           = None
     is_reading_frames  = False
     slots              = []
+    tries              = 0
 
     prev_len           = -1
     prev_checksum      = -1
 
+    tic = time.time()
     while True:
         if not nrf.data_ready():
+            tac = time.time()
+            
+            if (tac - tic) > CHANNEL_PERMANENCE_TIMEOUT:
+                WARN(f"Time-out of {CHANNEL_PERMANENCE_TIMEOUT} s while receiving frames, scanning channels again")
+                channel, channel_idx = choose_occupied_channel(nrf, other_channels, channel_idx + 1)
+                nrf.set_channel(channel)
+
             continue
 
-        if not tic_started:
-            tic = time.time()
-            tic_started = True
-
         frame: bytes = nrf.get_payload()
-        FrameID = int.from_bytes(frame[0:2])
+        FrameID = frame[0]
 
-        if FrameID == 0xFFFF:
-            data_len = int.from_bytes(frame[2:5])
-            checksum = frame[5:]
+        if FrameID == 0xFF:
+            data_len = int.from_bytes(frame[1:3])
+            checksum = frame[3:]
             
             if (
                 data_len != prev_len
@@ -356,22 +439,27 @@ def ACT_AS_RX(nrf: NRF24, other_channels: list[int]) -> tuple[bytes, float]:
                 INFO(f"Parsed header message: File length: {data_len} B | Checksum: {checksum.hex()}")
 
 
-        if is_reading_frames and (FrameID < num_of_frames):
-            slots[FrameID] = frame[2:]
+        if is_reading_frames and (FrameID < 0xFF):
+            slots[FrameID] = frame[1:]
     
 
         if is_reading_frames and (FrameID == num_of_frames - 1):
-            computed_checksum = shake_256(b"".join(slots)).digest(27)
+            computed_checksum = shake_256(b"".join(slots)).digest(29)
 
             if computed_checksum == checksum:
-                tac = time.time()
-                elapsed = tac - tic
                 SUCC("The checksum is correct")
-                INFO(f"RF throughput: {len(b''.join(slots)) / elapsed / 1024:.2f} KiBps | Elapsed time: {elapsed:.2f} s | Received bytes: {len(b''.join(slots))} B")
-                return b"".join(slots), elapsed
+                return b"".join(slots)
 
             else:
                 WARN("The checksum is incorrect, retrying")
+                
+                tries += 1
+                if tries >= PERSEVERANCE:
+                    WARN("Detected bad channel, scanning again")
+                    channel, channel_idx = choose_occupied_channel(nrf, other_channels, channel_idx + 1)
+                    nrf.set_channel(channel)
+                    
+        tic = time.time()
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
@@ -383,6 +471,7 @@ def main(nrf: NRF24, node_id: str, is_first_node: bool) -> None:
     """
     Main flow of the application
     """
+    # own_channels, other_channels = get_channels_based_on_node_id(node_id)
     all_channels                 = [15,45,76,90]
     own_channels, other_channels = get_channels_based_on_node_id(all_channels, node_id)
 
@@ -390,20 +479,18 @@ def main(nrf: NRF24, node_id: str, is_first_node: bool) -> None:
     INFO(f"RX channels: {other_channels}")
 
     if is_first_node:
-        file_path = get_file_to_transmit()
+        file_path = handle_tx_file_based_on_node_id(node_id)
         if not file_path: return
         
         content = file_path.read_bytes()
-        compressed_content = compress_file(content)
-        ACT_AS_TX(nrf, compressed_content, own_channels)
+        ACT_AS_TX(nrf, node_id, content, own_channels, is_first_node)
 
     else:
-        content, elapsed = ACT_AS_RX(nrf, other_channels)
-        content          = decompress_file(content)
-        INFO(f"Data throughput: {len(content) / elapsed / 1024:.2f} KiBps | Elapsed time: {elapsed:.2f} s | Received bytes: {len(content)} B")
-        file_path        = Path(FILE_TO_RX_STR).resolve()
+        content   = ACT_AS_RX(nrf, other_channels)
+        file_path = Path(RECEIVED_FILE_NAME)
         file_path.write_bytes(content)
-        SUCC(f"File successfully saved to: {file_path}")
+       
+        ACT_AS_TX(nrf, node_id, content, own_channels, is_first_node)
     return
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -421,3 +508,4 @@ if __name__ == "__main__":
 
     finally:
         if nrf is not None: nrf.power_down()
+        
